46afac4eb681bfa5fc2f55401123571b78e7d878
Trương Thành Ä?ại <124680705+thanhdaibl1572002@users.noreply.github.com>
2024-11-09 11:19:19 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,16 +9,6 @@ def split_data(df, target, test_size=0.2, random_state=42):
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=random_state)
     return X_train, X_test, Y_train, Y_test
 
-def evaluate_minmaxscaler(df, my_model, sk_model):
-    X_train = df
-    start_time = time.time()
-    my_model.fit_transform(X_train)
-    my_time = time.time() - start_time
-    start_time = time.time()
-    sk_model.fit_transform(X_train)
-    sk_time = time.time() - start_time
-    print(f"{'Time':<5} {my_time:<25} {sk_time:<25}")
-
 def evaluate_regression(df, target, my_model, sk_model):
     X_train, X_test, Y_train, Y_test = split_data(df, target=target)
     start_time = time.time()
@@ -47,3 +37,13 @@ def evaluate_classification(df, target, my_model, sk_model):
     print(f"{'Time':<5} {my_time:<25} {sk_time:<25}")
     print(f"{'Acc':<5} {accuracy_score(Y_test, my_Y_pred):<25} {accuracy_score(Y_test, sk_Y_pred):<25}")
     print(f"{'F1':<5} {f1_score(Y_test, my_Y_pred, average='weighted'):<25} {f1_score(Y_test, sk_Y_pred, average='weighted'):<25}")
+    
+def evaluate_minmaxscaler(df, my_model, sk_model):
+    X_train = df
+    start_time = time.time()
+    my_model.fit_transform(X_train)
+    my_time = time.time() - start_time
+    start_time = time.time()
+    sk_model.fit_transform(X_train)
+    sk_time = time.time() - start_time
+    print(f"{'Time':<5} {my_time:<25} {sk_time:<25}")
