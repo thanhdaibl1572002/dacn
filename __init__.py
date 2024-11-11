daa219765869bda814f542be2b7065fad557f100
Trương Thành Ä?ại <124680705+thanhdaibl1572002@users.noreply.github.com>
2024-11-11 21:18:12 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 import time
+import re
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, root_mean_squared_error, r2_score
 from sklearn.metrics import accuracy_score, f1_score
+from IPython.display import display, Markdown
 
 def split_data(df, target, test_size=0.2, random_state=42):
     X = df.drop(columns=[target]).values
@@ -19,6 +21,7 @@ def evaluate_regression(df, target, my_model, sk_model):
     sk_model.fit(X_train, Y_train)
     sk_Y_pred = sk_model.predict(X_test)
     sk_time = time.time() - start_time
+    display(Markdown(f"**{re.findall('[A-Z][a-z]*', type(my_model).__name__)}**"))
     print(f"{'':<5} {'My Model':<25} {'SK Model':<25}")
     print(f"{'Time':<5} {my_time:<25} {sk_time:<25}")
     print(f"{'MAE':<5} {mean_absolute_error(Y_test, my_Y_pred):<25} {mean_absolute_error(Y_test, sk_Y_pred):<25}")
@@ -35,6 +38,7 @@ def evaluate_classification(df, target, my_model, sk_model):
     sk_model.fit(X_train, Y_train)
     sk_Y_pred = sk_model.predict(X_test)
     sk_time = time.time() - start_time
+    display(Markdown(f"**{re.findall('[A-Z][a-z]*', type(my_model).__name__)}**"))
     print(f"{'':<5} {'My Model':<25} {'SK Model':<25}")
     print(f"{'Time':<5} {my_time:<25} {sk_time:<25}")
     print(f"{'Acc':<5} {accuracy_score(Y_test, my_Y_pred):<25} {accuracy_score(Y_test, sk_Y_pred):<25}")
@@ -48,5 +52,6 @@ def evaluate_minmaxscaler(df, my_model, sk_model):
     start_time = time.time()
     sk_model.fit_transform(X_train)
     sk_time = time.time() - start_time
+    display(Markdown(f"**{re.findall('[A-Z][a-z]*', type(my_model).__name__)}**"))
     print(f"{'':<5} {'My Model':<25} {'SK Model':<25}")
     print(f"{'Time':<5} {my_time:<25} {sk_time:<25}")
