18905e8b178cabbf11f75d90e2da01b30358be91
Trương Thành Ä?ại <124680705+thanhdaibl1572002@users.noreply.github.com>
2024-11-15 08:10:49 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 import time
 import re
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -9,44 +8,88 @@ from sklearn.metrics import accuracy_score, f1_score, classification_report
 from IPython.display import display, Markdown
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-    
-# def evaluate_minmaxscaler(df, my_model, sk_model):
-#     X_train = df
-#     start_time = time.time()
-#     my_model.fit_transform(X_train)
-#     my_time = time.time() - start_time
-#     start_time = time.time()
-#     sk_model.fit_transform(X_train)
-#     sk_time = time.time() - start_time
-#     display_model_name(my_model)
-#     print(f"{'':<5} {'My Model':<25} {'SK Model':<25}")
-#     print(f"{'Time':<5} {my_time:<25} {sk_time:<25}")
-    
+
+# =============================== UTILS =============================== #
 def split_data(df, target, test_size=0.2, random_state=42):
     X = df.drop(columns=[target]).values
     Y = df[target].values
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=random_state)
     return X_train, X_test, Y_train, Y_test
 
+def get_model_name(model):
+    return ' '.join(re.findall(r'[A-Z][a-z]*', type(model).__name__))
+
+# =============================== SCALER =============================== #
+def scaler_data():
+    datasets = {
+        "Scaler Regression Big": pd.read_csv('dacn/regression_big.csv'),
+        "Scaler Classification Big": pd.read_csv('dacn/classification_big.csv'),
+    }
+    return datasets
+
+def scaler_evaluate(df, my_model, sk_model):
+    start_time = time.time()
+    my_model_scaled = my_model.fit_transform(df)
+    print(f"My Model Scaler [0]: \n{my_model_scaled[0]}\n")
+    my_time = time.time() - start_time
+    start_time = time.time()
+    sk_model_scaled = sk_model.fit_transform(df)
+    print(f"SK Model Scaler [0]: \n{sk_model_scaled[0]}\n")
+    sk_time = time.time() - start_time
+    my_time_s = f"{my_time:.3f} s"
+    sk_time_s = f"{sk_time:.3f} s"
+    mae = f"{mean_absolute_error(my_model_scaled, sk_model_scaled):.3f}"
+    rmse = f"{root_mean_squared_error(my_model_scaled, sk_model_scaled):.3f}"
+    r2 = f"{r2_score(my_model_scaled, sk_model_scaled):.3f}"
+    print(f"{'':<5} {'My Model':<25} {'SK Model':<25}")
+    print(f"{'Time':<5} {my_time_s:<25} {sk_time_s}")
+    print(f"MAE, RMSE, R2: {mae}, {rmse}, {r2}")
+    return my_time, sk_time, mae, mae, rmse, r2
+
+def scaler_evaluates(my_model, sk_model):
+    datasets = scaler_data()
+    my_times = []
+    sk_times = []
+    dataset_names = []
+    model_name = get_model_name(my_model)
+    for dataset_name, df in datasets.items():
+        display(Markdown(f"**{model_name} - {dataset_name}**"))
+        results = scaler_evaluate(df, my_model, sk_model)
+        my_times.append(results[0])
+        sk_times.append(results[1])
+        dataset_names.append(dataset_name)
+    display(Markdown(f"**{model_name} - Time Comparison**"))
+    fig, ax = plt.subplots(figsize=(10, 6))
+    x = range(len(dataset_names))
+    ax.bar(x, my_times, width=0.4, label='My Model', align='center', color='skyblue')
+    ax.bar([i + 0.4 for i in x], sk_times, width=0.4, label='SK Model', align='center', color='lightgreen')
+    ax.set_xlabel("Dataset Size")
+    ax.set_ylabel("Time (seconds)")
+    ax.set_title("Time Comparison of My Model vs SK Model on Different Datasets")
+    ax.set_xticks([i + 0.2 for i in x])
+    ax.set_xticklabels(dataset_names)
+    ax.legend()
+    plt.show()
+
 # =============================== CLASSIFICATION =============================== #
 def classification_data():
-    classification_datasets = {
+    datasets = {
         "Classification Big": pd.read_csv('dacn/classification_big.csv'),
         "Classification Medium": pd.read_csv('dacn/classification_medium.csv'),
         "Classification Small": pd.read_csv('dacn/classification_small.csv')
     }
-    classification_new = pd.read_csv('dacn/classification_new.csv')
-    classification_target = 'revenue'
+    new = pd.read_csv('dacn/classification_new.csv')
+    target = 'revenue'
     scaler = StandardScaler()
-    for key in classification_datasets:
-        X = classification_datasets[key].drop(columns=[classification_target])
-        y = classification_datasets[key][classification_target]
+    for key in datasets:
+        X = datasets[key].drop(columns=[target])
+        y = datasets[key][target]
         X_scaled = scaler.fit_transform(X)
-        classification_datasets[key] = pd.DataFrame(X_scaled, columns=X.columns)
-        classification_datasets[key][classification_target] = y
-    X_new_scaled = scaler.transform(classification_new)
-    classification_new = pd.DataFrame(X_new_scaled, columns=classification_new.columns)
-    return classification_datasets, classification_new, classification_target
+        datasets[key] = pd.DataFrame(X_scaled, columns=X.columns)
+        datasets[key][target] = y
+    X_new_scaled = scaler.transform(new)
+    new = pd.DataFrame(X_new_scaled, columns=new.columns)
+    return datasets, new, target
 
 def classification_evaluate(df, target, my_model, sk_model):
     X_train, X_test, Y_train, Y_test = split_data(df, target=target)
@@ -90,7 +133,7 @@ def classification_evaluates(my_model, sk_model):
     my_times = []
     sk_times = []
     dataset_names = []
-    model_name = ' '.join(re.findall(r'[A-Z][a-z]*', type(my_model).__name__))
+    model_name = get_model_name(my_model)
     for dataset_name, df in datasets.items():
         display(Markdown(f"**{model_name} - {dataset_name}**"))
         my_time, sk_time = classification_evaluate(df, target, my_model, sk_model)
@@ -110,26 +153,34 @@ def classification_evaluates(my_model, sk_model):
     ax.legend()
     plt.show()
     
+def classification_predictions(my_model):
+    _, new, target = classification_data()
+    predictions = my_model.predict(new)
+    new_with_predictions = new.copy()
+    new_with_predictions[target + " (Predicted)"] = predictions
+    model_name = get_model_name(my_model)
+    display(Markdown(f"**{model_name} - Predictions On New Data**"))
+    return new_with_predictions
     
 # =============================== REGRESSION =============================== #
 def regression_data():
-    regression_datasets = {
+    datasets = {
         "Regression Big": pd.read_csv('dacn/regression_big.csv'),
         "Regression Medium": pd.read_csv('dacn/regression_medium.csv'),
         "Regression Small": pd.read_csv('dacn/regression_small.csv')
     }
-    regression_new = pd.read_csv('dacn/regression_new.csv')
-    regression_target = 'rul'
+    new = pd.read_csv('dacn/regression_new.csv')
+    target = 'rul'
     scaler = StandardScaler()
-    for key in regression_datasets:
-        X = regression_datasets[key].drop(columns=[regression_target])
-        y = regression_datasets[key][regression_target]
+    for key in datasets:
+        X = datasets[key].drop(columns=[target])
+        y = datasets[key][target]
         X_scaled = scaler.fit_transform(X)
-        regression_datasets[key] = pd.DataFrame(X_scaled, columns=X.columns)
-        regression_datasets[key][regression_target] = y
-    X_new_scaled = scaler.transform(regression_new)
-    regression_new = pd.DataFrame(X_new_scaled, columns=regression_new.columns)
-    return regression_datasets, regression_new, regression_target
+        datasets[key] = pd.DataFrame(X_scaled, columns=X.columns)
+        datasets[key][target] = y
+    X_new_scaled = scaler.transform(new)
+    new = pd.DataFrame(X_new_scaled, columns=new.columns)
+    return datasets, new, target
 
 def regression_evaluate(df, target, my_model, sk_model):
     X_train, X_test, Y_train, Y_test = split_data(df, target=target)
@@ -154,7 +205,7 @@ def regression_evaluate(df, target, my_model, sk_model):
     print(f"{'MAE':<5} {my_mae:<25} {sk_mae}")
     print(f"{'RMSE':<5} {my_rmse:<25} {sk_rmse}")
     print(f"{'R2':<5} {my_r2:<25} {sk_r2}")
-    return my_time, sk_time
+    return my_time, sk_time, my_mae, sk_mae, my_rmse, sk_rmse, my_r2, sk_r2
 
 def regression_evaluates(my_model, sk_model):
     datasets, _, target = regression_data()
@@ -167,7 +218,7 @@ def regression_evaluates(my_model, sk_model):
     my_r2s = []
     sk_r2s = []
     dataset_names = []
-    model_name = ' '.join(re.findall(r'[A-Z][a-z]*', type(my_model).__name__))
+    model_name = get_model_name(my_model)
     for dataset_name, df in datasets.items():
         display(Markdown(f"**{model_name} - {dataset_name}**"))
         results = regression_evaluate(df, target, my_model, sk_model)
@@ -225,3 +276,12 @@ def regression_evaluates(my_model, sk_model):
     ax.set_xticklabels(dataset_names)
     ax.legend()
     plt.show()
+    
+def regression_predictions(my_model):
+    _, new, target = regression_data()
+    predictions = my_model.predict(new)
+    new_with_predictions = new.copy()
+    new_with_predictions[target + " (Predicted)"] = predictions
+    model_name = get_model_name(my_model)
+    display(Markdown(f"**{model_name} - Predictions on New Data**"))
+    return new_with_predictions
